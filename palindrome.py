1883460b03b8a6ea889d865881bef94b1a214034
Amnnnn16 <ag6835902@gmail.com>
2024-04-24 23:03:25 +0530

    submitted

---
+++ palindrome.py
@@ -0,0 +1,7 @@
+import re
+
+def is_palindrome(s):
+    # Convert string to lowercase and remove non-alphanumeric characters
+    s = re.sub(r'[^a-zA-Z0-9]', '', s.lower())
+    # Check if the string is equal to its reverse
+    return s == s[::-1]
